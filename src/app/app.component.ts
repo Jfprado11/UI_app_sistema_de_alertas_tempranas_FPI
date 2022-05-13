06edff40dd794316b119fd5b420bad5521c0fed7
Felipe Prado <jfpc11@misena.edu.co>
2022-05-13 10:18:23 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'UI_app_sistema_de_alertas_tempranas_FPI';
+}
